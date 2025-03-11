340fa76c82c714d9270f6c26e28303315bb286f6
XYZLassi <12987399+XYZLassi@users.noreply.github.com>
2025-03-11 07:42:26 +0100

    Update {{cookiecutter.presenter_base_name}}.py

--- {{cookiecutter.base_name}}/{{cookiecutter.presenter_base_name}}.py
+++ {{cookiecutter.base_name}}/{{cookiecutter.presenter_base_name}}.py
@@ -3,10 +3,16 @@ from .protocols import {{cookiecutter.__protocol_name}}, {{cookiecutter.__presen
 
 class {{cookiecutter.presenter_name}}({{cookiecutter.__presenter_protocol_name}}):
 	def __init__(self, view: {{cookiecutter.__protocol_name}}):
-		self.view = view
+		self.__view : {{cookiecutter.__protocol_name}} | None = view
 		
 		self.view.create_ui(self)
 	
+	@property
+    	def view(self) -> {{cookiecutter.__protocol_name}}:
+        	if self.__view is None:
+            		raise RuntimeError('View is not initialized')
+        	return self.__view
+
 	def on_close(self):
 		self.close()
 	
