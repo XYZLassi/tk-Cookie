9d11be21ad3e557916fd32c22874a46b67d68272
XYZLassi <12987399+XYZLassi@users.noreply.github.com>
2025-04-22 06:30:24 +0200

    Update {{cookiecutter.base_name}}.py
    Add: # noinspection PyAttributeOutsideInit, DuplicatedCode

--- {{cookiecutter.base_name}}/{{cookiecutter.base_name}}.py
+++ {{cookiecutter.base_name}}/{{cookiecutter.base_name}}.py
@@ -6,7 +6,8 @@ from .protocols import {{cookiecutter.__protocol_name}}, {{cookiecutter.__presen
 class {{cookiecutter.name}}({{cookiecutter.__type}}, {{cookiecutter.__protocol_name}}):
 	def __init__(self, parent):
 		super().__init__(parent)
-	
+
+	# noinspection PyAttributeOutsideInit, DuplicatedCode
 	def create_ui(self, presenter: {{cookiecutter.__presenter_protocol_name}}):
 		# General
 		{%- if cookiecutter.type == "Toplevel"%}
